c685efbbf4c3d1876d4de8e5104bf31a6ade0c00
Demetria Smith <demetriacsmith@gmail.com>
2016-07-01 13:54:30 -0500

    lots of changes- I know this is a horrible message but it is 1:54

--- js/lemonadeStand.js
+++ js/lemonadeStand.js
@@ -8,62 +8,62 @@ function simulate(){
 	var numberPeople=document.getElementById("people").value;
 	var pricePerCup=document.getElementById("price").value;
 	var costPerCup=document.getElementById("cost").value;
-	//console.log(days,numberPeople,pricePerCup,costPerCup);
 	var theWeather = new Weather();
 	theWeather.retrieveWeather(days);
 	var theBestStand = new LemonadeStand();
-	var individual=new Person();
-	individual.addIndividuals(numberPeople);
-	//theBestStand.determineBuyers(theWeather.todaysHigh);
+	theBestStand.countEveryLastPerson(days,numberPeople,theWeather.Forecast[0],pricePerCup);
 	theBestStand.getGlassesSold();
-	theBestStand.calculateProfit(pricePerCup,costPerCup);
-	theBestStand.makeOutputTable(days,numberPeople,numberOfGlassesSold,profit);
-
+	console.log(glassesSoldPerDay);
+	//theBestStand.calculateProfit(pricePerCup,costPerCup);
+	//theBestStand.makeOutputTable(days,numberPeople,numberOfGlassesSold,profit);
 }
 
 function Weather(){ 	
-	this.getForecast=[];
+	this.Forecast=[];
 	this.retrieveWeather= function(days){  //get days from user input 
 		for(var i=0; i<days; i++){
 			var todaysHigh=document.weather.forecast[i].high;
-			this.getForecast.push(todaysHigh);
+			this.Forecast.push(todaysHigh);
+			//console.log(todaysHigh);
 		}
 	}
 }
 
 function Person() {
-	this.baseLikelihood=Math.random();
-	this.determineLikelihood=function(todaysHigh,pricePerCup){	
-		var likelihood = todaysHigh > 75 ? (todaysHigh%75)*this.baseLikelihood/pricePerCup : this.baseLikelihood/pricePerCup;
+	this.purchased=Math.random();
+	this.doIBuy=function(temperature,pricePerCup){
+		var likelihood = temperature> 75 ? (temperature%75)*this.purchased/pricePerCup : this.purchased/pricePerCup;
+		this.purchased=likelihood>0.5;
+		return likelihood>0.5;
 	}
-}	
-
-
+}
+	
 function LemonadeStand(){
-	this.peopleArray=[];
-	this.likelihoodArray=[];
-	this.addIndividuals=function(numberPeople){ //we get numberPeople from the user input
-		for (var i=numberPeople; i>0; i--){  
-			//create an instance of the person class
-			var newIndividual=new Person();
-
-			//then push that instance into the peopleArray
-			this.peopleArray.push(newIndividual);
+	this.countEveryLastPerson=function(days,numberPeople,temperature,pricePerCup){
+		this.everybody=[];
+		for(var i=0; i<days; i++){
+			this.peopleArray=[];
+				for (var d=0;d<numberPeople; d++){  
+					var newIndividual=new Person(); //create an instance of the person class
+					newIndividual.doIBuy(temperature,pricePerCup);					
+					this.peopleArray.push(newIndividual);//then push that instance into the peopleArray
+				}
+			this.everybody.push(this.peopleArray);	
 		}
 	}
-
 
 	this.getGlassesSold=function (){
-		function isTrue(person){
-			return person.determineLikelihood() > 0.5;
+		this.glassesSoldPerDay=[];
+		for(var i=0;i<this.everybody.length;i++){
+			var x=everybody[i].filter;
+			var y=x.length;
+			glassesSoldPerDay.push(y);
 		}
-
-		var glassesSold=this.peopleArray.filter(isTrue);//doesn't know where to find this likelihood array
-		var numberOfGlassesSold= glassesSold.length;
 	}
+	
 
 	this.calculateProfit=function(pricePerCup,costPerCup){
-		var profit=pricePerCup-costPerCup;
+		this.profit=pricePerCup-costPerCup;//this is only taking the profit from selling one cup
 	}
 
 	this.makeOutputTable=function(days,numberPeople,numberOfGlassesSold,profit){
@@ -80,10 +80,57 @@ function LemonadeStand(){
 		var findIDforOutputTable=document.getElementById("outputTable");
 		findIDforOutputTable.innerHTML=OutputHTML;
 
-	}
-}			
+	}	
+}		
 
 
-
-
+	//this.peopleArray=[];
+	/*
+	this.customersArray=[];//should only include people who actually bought lemonade
 	
+	this.addCustomers= function(theWeather,pricePerCup){
+		for(var i=0; i<this.peopleArray.length; i++){
+		var likelihood = theWeather.Forecast[i]> 75 ? (theWeather%75)*this.peopleArray[i].purchased/pricePerCup : this.peopleArray[i].purchased/pricePerCup;
+		//console.log(likelihood);
+		var customer= likelihood> 0.5 
+		this.customersArray.push(customer);
+		//console.log(this.customersArray);
+		}
+	}	
+*/
+	/*this.addIndividuals=function(numberPeople,temperature,pricePerCup){ 
+		for (var i=numberPeople; i>0; i--){  
+			//create an instance of the person class
+			var newIndividual=new Person();
+			newIndividual.doIBuy(temperature,pricePerCup);
+
+			//then push that instance into the peopleArray
+			this.peopleArray.push(newIndividual);
+		}
+	}*/
+
+/*function countEveryLastPerson(days,numberPeople,temperature,pricePerCup){
+	this.everybody=[];
+	for(var=0, i<days,i++){
+		this.peopleArray=[];
+		this.addIndividuals=function(numberPeople,temperature,pricePerCup){ 
+			for (var d=numberPeople; d>0; d--){  
+				//create an instance of the person class
+				var newIndividual=new Person();
+				newIndividual.doIBuy(temperature,pricePerCup);
+
+				//then push that instance into the peopleArray
+				this.peopleArray.push(newIndividual);
+				var thisDaysPeople=this.PeopleArray;
+			}
+		}
+	this.everybody.push(thisDaysPeople);//this variable should be an array of the people for that day
+	}
+}*/
+
+/*var individual=new Person();
+var stand=new LemonadeStand();
+stand.addIndividuals(3);
+console.log(stand.peopleArray);
+stand.addCustomers(75,1);*/
+
